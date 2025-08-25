235facb1d365befbb8ef3003a93f1fcc8dcf37d6
Päpke, Tim Carlo <tim.paepke@stud.tu-darmstadt.de>
2025-08-25 17:11:55 +0200

    moved Handshake to external files

---
+++ common/include/handshake.h
@@ -0,0 +1,78 @@
+#ifndef HANDSHAKE_H
+#define HANDSHAKE_H
+
+#if defined(__MSP430FR5994__)
+#include "msp430fr5994_helper.h"
+#include "msp430fr5994_gpio.h"
+#include "msp430fr5994_utils.h"
+#include "msp430fr5994_time.h"
+#include "printf.h"
+#include <stdbool.h>
+#include <stdint.h>
+#ifndef NUMBER_OF_GPIO_PINS
+#define NUMBER_OF_GPIO_PINS MSP430_NUM_ABS_PINS
+#endif
+
+#define DEBUG_PIN1 ABS_PIN(3, 4) // Pin used for debugging, can be changed as needed
+#define DEBUG_PIN2 ABS_PIN(3, 5) // Pin used for debugging, can be changed as needed
+#define DEBUG_PIN3 ABS_PIN(8, 1) // Additional debug pin, can be changed as needed
+#define DEBUG_PIN4 ABS_PIN(8, 2) // Additional debug pin, can be changed as needed
+#elif defined(NRF52840_XXAA)
+#include "nrf52840.h"
+#include "nrf52840_helper.h"
+#include "nrf52840_utils.h"
+#include "nrf52840_gpio.h"
+#include "nrf52840_time.h"
+#ifndef NUMBER_OF_GPIO_PINS
+#define NUMBER_OF_GPIO_PINS NRF52_NUM_ABS_PINS
+#endif
+
+#define DEBUG_PIN1 26 // Pin used for debugging, can be changed as needed
+#define DEBUG_PIN2 27 // Pin used for debugging, can be changed as needed
+#define DEBUG_PIN3 39 // Additional debug pin, can be changed as needed
+#define DEBUG_PIN4 40 // Additional debug pin, can be changed as needed
+
+#endif
+
+#include "printf.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include "timing_pindata.h"
+#include "bitmap_iterator.h"
+
+#define READER_INTERVAL_US 1000   // Reader: every 1 ms
+#define MANAGER_INTERVAL_US 10000 // Manager: every 10 ms
+#define PRESCALER_DIV 8           // Prescaler division factor for the timer
+#define READER_TICKS ((READER_INTERVAL_US * (SMCLK_HZ / PRESCALER_DIV / 1000000UL)))
+#define MANAGER_TICKS ((MANAGER_INTERVAL_US * (SMCLK_HZ / PRESCALER_DIV / 1000000UL)))
+
+#define SYN_DURATION 200     // Duration of SYN signal in microseconds
+#define SYN_ACK_DURATION 600 // Duration of SYN_ACK signal in microseconds
+#define ACK_DURATION 1100    // Duration of ACK signal in microseconds
+
+#define SYN_CYCLES ((uint32_t)SYN_DURATION * 1000UL / READER_INTERVAL_US)
+#define SYN_ACK_CYCLES ((uint32_t)SYN_ACK_DURATION * 1000UL / READER_INTERVAL_US)
+#define ACK_CYCLES ((uint32_t)ACK_DURATION * 1000UL / READER_INTERVAL_US)
+
+#define SIGNAL_INACCURACY 100 // Signal inaccuracy in milliseconds
+#define CYCLE_INACCURACY ((uint32_t)SIGNAL_INACCURACY * 1000UL / READER_INTERVAL_US)
+#define MAXIMUM_WAITING_CYCLES 500 // Maximum waiting cycles for a signal
+
+#define MIN_SYN_CYCLES ((uint32_t)(SYN_DURATION - SIGNAL_INACCURACY) * 1000UL / READER_INTERVAL_US)
+#define MIN_SYN_ACK_CYCLES ((uint32_t)(SYN_ACK_DURATION - SIGNAL_INACCURACY) * 1000UL / READER_INTERVAL_US)
+#define MIN_ACK_CYCLES ((uint32_t)(ACK_DURATION - SIGNAL_INACCURACY) * 1000UL / READER_INTERVAL_US)
+
+#define MAXIMUM_SYN_CYCLES ((uint32_t)(SYN_DURATION + SIGNAL_INACCURACY) * 1000UL / READER_INTERVAL_US)
+#define MAXIMUM_SYN_ACK_CYCLES ((uint32_t)(SYN_ACK_DURATION + SIGNAL_INACCURACY) * 1000UL / READER_INTERVAL_US)
+#define MAXIMUM_ACK_CYCLES ((uint32_t)(ACK_DURATION + SIGNAL_INACCURACY) * 1000UL / READER_INTERVAL_US)
+typedef struct
+{
+    uint16_t cycles;
+    void (*on_complete)(PinData *);
+} TaskDef;
+
+void perform_handshake(PinData *pin_data, uint64_t initial_blacklist_mask);
+
+#endif // HANDSHAKE_H
---
+++ common/utils/handshake.c
@@ -0,0 +1,200 @@
+#include "handshake.h"
+
+static volatile uint64_t initial_state_mask = 0; // Mask to store the initial state of pins
+static PinData *global_pin_data = NULL; // Global pointer to PinData array
+
+static void on_syn_complete(PinData *data)
+{
+    // SYN task completed, prepare for SYN_ACK
+    set_syn(data, true);           // Set SYN flag
+    set_initiator_syn(data, true); // Mark as initiator for SYN
+    data->sending_counter = 0;     // Reset sending counter for next task
+}
+static void on_syn_ack_complete(PinData *data)
+{
+    // SYN_ACK task completed, prepare for ACK
+    set_syn_ack(data, true);          // Set SYN_ACK flag
+    set_initiator_synack(data, true); // Mark as initiator for SYN_ACK
+    data->sending_counter = 0;        // Reset sending counter for next task
+}
+static void on_ack_complete(PinData *data)
+{
+    // ACK task completed, reset task to none
+    set_ack(data, true);
+    set_initiator_ack(data, true); // Mark as initiator for ACK
+    data->sending_counter = 0;     // Reset sending counter
+    if (is_successful(data))
+    {
+        data->successfull_handshakes++; // Increment successful handshakes counter
+        if (data->successfull_handshakes >= 5)
+        {
+            initial_state_mask |= (1ULL << data->pin);
+        }
+    }
+}
+
+const TaskDef task_lut[] = {
+    [TASK_NONE] = {0, NULL},
+    [TASK_JOB_SYN] = {SYN_CYCLES, &on_syn_complete},
+    [TASK_JOB_SYN_ACK] = {SYN_ACK_CYCLES, &on_syn_ack_complete},
+    [TASK_JOB_ACK] = {ACK_CYCLES, &on_ack_complete}};
+
+static void reader_isr(void)
+{
+    gpio_drive_high(DEBUG_PIN1); // Set debug pin high to indicate ISR entry
+    uint64_t all_pins_mask = (NUMBER_OF_GPIO_PINS >= 64)
+                                 ? ~0ULL
+                                 : ((1ULL << NUMBER_OF_GPIO_PINS) - 1);
+
+    BitmapIterator it = bitmap_iterator_create(~initial_state_mask & all_pins_mask);
+
+    uint8_t pin;
+    while (bitmap_iterator_next(&it, &pin))
+    {
+        PinData *data = &global_pin_data[pin];
+
+        bool something_done = false; // Flag to track if any task was done
+
+        if (data->waiting_counter >= MAXIMUM_WAITING_CYCLES)
+        {
+            set_task(data, TASK_JOB_SYN);
+            data->waiting_counter = 0; // Reset waiting counter
+        }
+
+        // Check if a task is conducted on this pin
+        PinDataTask task = get_task(data);
+        bool pin_state = gpio_read(pin);
+
+        if (task != TASK_NONE)
+        {
+            something_done = true; // At least one task is being done
+            data->waiting_counter = 0;
+            //  Collision detection:
+            //  Before sending, check if the pin is high and we are sending
+            if (data->sending_counter == 0 && !pin_state)
+            {
+                set_task(data, TASK_NONE); // Reset task
+            }
+            else
+            {
+                uint16_t required_cycles = task_lut[task].cycles;
+                if (data->sending_counter >= required_cycles)
+                {
+                    // Reset when task is complete
+                    gpio_od_release(pin); // Release the pin
+                    set_task(data, TASK_NONE);
+                    task_lut[task].on_complete(data);
+                }
+                else
+                {
+                    gpio_od_hold_low(pin);   // Drive pin low to send signal
+                    data->sending_counter++; // Increment cycle counter
+                }
+            }
+        }
+        else
+        {
+            if (!pin_state) // if a signal is received
+            {
+                something_done = true;     // At least one task is being done
+                data->receiving_counter++; // Increment receiving counter
+            }
+        }
+        if (pin_state) // if no signal is received, reset the receiving counter
+        {
+            uint16_t receive_counter = data->receiving_counter;
+            if (receive_counter >= MIN_SYN_CYCLES && receive_counter <= MAXIMUM_SYN_CYCLES)
+            {
+                // SYN signal detected
+                set_syn(data, true);
+                set_initiator_syn(data, false); // Mark as responder for SYN
+                set_task(data, TASK_JOB_SYN_ACK);
+                data->receiving_counter = 0; // Reset receiving counter after SYN
+                something_done = true;       // Mark that something was done
+            }
+            else if (receive_counter >= MIN_SYN_ACK_CYCLES && receive_counter <= MAXIMUM_SYN_ACK_CYCLES)
+            {
+                // SYN_ACK signal detected
+                set_syn_ack(data, true);
+                set_initiator_synack(data, false); // Mark as responder for SYN_ACK
+                set_task(data, TASK_JOB_ACK);
+                data->receiving_counter = 0; // Reset receiving counter after SYN_ACK
+                something_done = true;       // Mark that something was done
+            }
+            else if (receive_counter >= MIN_ACK_CYCLES && receive_counter <= MAXIMUM_ACK_CYCLES)
+            {
+                // ACK signal detected
+                set_ack(data, true);
+                set_initiator_ack(data, false); // Mark as responder for ACK
+                data->receiving_counter = 0;    // Reset receiving counter after ACK
+                something_done = true;          // Mark that something was done
+                if (is_successful(data))
+                {
+                    data->successfull_handshakes++; // Increment successful handshakes counter
+                    if (data->successfull_handshakes >= 5)
+                    {
+                        initial_state_mask |= (1ULL << pin);
+                    }
+                }
+            }
+            else if (receive_counter > MAXIMUM_ACK_CYCLES)
+            {
+                // Signal too long, reset counters
+                data->receiving_counter = 0;
+            }
+        }
+
+        if (!something_done)
+        {
+            data->waiting_counter++; // Increment waiting counter if no task was done
+        }
+
+    }
+    gpio_drive_low(DEBUG_PIN1); // Set debug pin low to indicate ISR exit
+}
+
+void start_isr_timer(void)
+{
+#if defined(NRF52840_XXAA)
+    configure_timer(NRF_TIMER4, 4, TIMER_BITMODE_BITMODE_32Bit);  // 1MHz (1µs per tick)
+    set_timer_compare(NRF_TIMER4, 0, READER_INTERVAL_US, true); // Set compare for 1ms intervals and enable interrupt
+    
+    // Enable auto-restart on compare
+    NRF_TIMER4->SHORTS = TIMER_SHORTS_COMPARE0_CLEAR_Msk;
+    
+    // Set callback and start
+    set_timer_event_callback(NRF_TIMER4, reader_isr);
+    start_timer(NRF_TIMER4);
+
+#elif defined(__MSP430FR5994__)
+    // Configure Timer A1 for periodic ISR calls
+    configure_timer(TIMER_A1, 8, MC__STOP);  // SMCLK/8, continuous mode
+    set_timer_compare(TIMER_A1, 0, READER_TICKS);
+    
+    // Set callback and start
+    set_timer_compare_callback(TIMER_A1, reader_isr);
+    start_timer_with_interrupt(TIMER_A1);
+#endif
+}
+
+void stop_isr_timer(void)
+{
+#if defined(NRF52840_XXAA)
+    clear_timer_event_callback(NRF_TIMER4);
+    stop_timer(NRF_TIMER4);
+    
+#elif defined(__MSP430FR5994__)
+    clear_timer_callbacks(TIMER_A1);
+    stop_timer(TIMER_A1);
+#endif
+}
+
+
+void perform_handshake(PinData *pin_data_array, uint64_t initial_blacklist_mask)
+{
+    initial_state_mask = initial_blacklist_mask;
+    global_pin_data = pin_data_array; 
+    start_isr_timer();
+    delay_ms(60000); // Run handshake for 60 seconds
+    stop_isr_timer();
+}
