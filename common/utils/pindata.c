862dac66630f1ad24d15f2544ddd469f179a837e
Päpke, Tim Carlo <tim.paepke@stud.tu-darmstadt.de>
2025-08-31 12:58:47 +0200

    add debug function

--- common/utils/pindata.c
+++ common/utils/pindata.c
@@ -1,4 +1,5 @@
 #include "pindata.h"
+#include "printf.h" 
 
 
 /**
@@ -27,4 +28,38 @@ void add_pin_event(PinData *pindata, uint8_t pin, PinEventType event)
     PinData *data = &pindata[pin];
     data->pin_event[data->event_index] = event;
     data->event_index = (data->event_index + 1) % EVENT_BUFFER_SIZE; // Circular buffer
+}
+
+/**
+ * @brief Print the pin data array and list events for each element
+ * 
+ * @param pindata Pointer to the PinData array
+ * @param size Size of the array
+ */
+void print_pin_data_array(const PinData *pindata, uint8_t size)
+{
+    for (uint8_t i = 0; i < size; ++i)
+    {
+        printf("Pin %u:\n", pindata[i].pin);
+        printf("  Event Index: %u\n", pindata[i].event_index);
+        printf("  Events: ");
+        for (uint8_t j = 0; j < EVENT_BUFFER_SIZE; ++j)
+        {
+            switch (pindata[i].pin_event[j]) {
+            case HANDSHAKE_OK_INITIATOR:
+                printf("HANDSHAKE_OK_INITIATOR ");
+                break;
+            case HANDSHAKE_OK_RESPONDER:
+                printf("HANDSHAKE_OK_RESPONDER ");
+                break;
+            case HANDSHAKE_FAILURE:
+                printf("HANDSHAKE_FAILURE ");
+                break;
+            default:
+                printf("%d ", pindata[i].pin_event[j]);
+                break;
+            }
+        }
+        printf("\n");
+    }
 }
