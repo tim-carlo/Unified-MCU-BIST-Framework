0ac9a630322898bae4e3cd50db6cf2f2f630478e
Päpke, Tim Carlo <tim.paepke@stud.tu-darmstadt.de>
2025-09-30 15:49:10 +0200

    Refactor pin data structure and update event types for clarity

--- common/include/pindata.h
+++ common/include/pindata.h
@@ -7,15 +7,18 @@
 
 #if defined(NRF52840_XXAA)
 #include "nrf52840_gpio.h"
+#include "nrf52840_utils.h"
 
 #elif defined(__MSP430FR5994__)
 #include "msp430fr5994_gpio.h"
+#include "msp430fr5994_utils.h"
 #define NUMBER_OF_GPIO_PINS MSP430_NUM_ABS_PINS
 #endif
 
 #define EVENT_BUFFER_SIZE 10
 #define INITIAL_CONNECTION_CAPACITY 1 // Initial capacity for connections array
 #define MAX_SEEN_DEVICES 5 // Maximum number of seen devices to track
+#define MY_DEVICE_ID_INDEX 0 // Index of own device in seen_devices arrays
 
 typedef enum
 {
@@ -27,7 +30,13 @@ typedef enum
     HANDSHAKE_FAILURE = 5,
     DATA_HANDSHAKE_OK = 6,
     DATA_HANDSHAKE_FAILURE = 7,
-    PIN_IS_CONNECTED_WITH_OTHER_PIN = 8,
+    PIN_IS_CONNECTED_WITH_INTERNAL_PIN = 8,
+    PIN_IS_CONNECTED_WITH_EXTERNAL_PIN = 9,
+    PIN_IS_NOT_LOW_WHEN_PULLED_DOWN = 10,
+    PIN_IS_NOT_HIGH_WHEN_PULLED_UP = 11,
+    PIN_IS_NOT_LOW_WHEN_DRIVEN_LOW = 12,
+    PIN_IS_NOT_HIGH_WHEN_DRIVEN_HIGH = 13
+
 } PinEventType;
 
 // Hier den Grunddatentype Typ definieren um das zu minimieren.
@@ -61,8 +70,8 @@ void add_pin_event(PinData *pindata, uint8_t pin, PinEventType event);
 
 
 uint8_t add_seen_device(uint64_t *other_device_id);
-bool connection_exists(PinData *data, uint8_t other_pin, uint8_t device_index);
-void add_pin_connection(PinData *pindata, uint8_t pin, uint8_t other_pin, uint64_t *other_device_id);
+uint8_t get_index_of_unique_id(uint64_t unique_id);
+void add_pin_connection(PinData *pindata, uint8_t pin, uint8_t other_pin_index, uint8_t device_index);
 
 void print_pin_data_array(const PinData *pindata, uint8_t size);
 
