b333671dee539bb7e5fde2fdea31deae291fd713
Päpke, Tim Carlo <tim.paepke@stud.tu-darmstadt.de>
2025-10-18 13:03:15 +0200

    Add retry logic and logging for mutex request and release operations

--- common/utils/mutex_handeler.c
+++ common/utils/mutex_handeler.c
@@ -4,6 +4,9 @@
 static bool iam_mutex_owner = false;
 static uint8_t current_mutex_pin = 255;
 static bool currently_having_mutex = false;
+
+static const uint8_t MAX_RELEASE_TRIES = 4;
+static const uint8_t MAX_REQUEST_TRIES = 4;
 /**
  * @brief Initialize the mutex handler
  *
@@ -30,8 +33,15 @@ void mutex_handler_request_mutex()
     if (iam_mutex_owner && !currently_having_mutex)
     {
         uint8_t request = MUTEX_REQEST;
+        uint8_t tries = 0;
         while (!currently_having_mutex)
         {
+            if (tries >= MAX_REQUEST_TRIES)
+            {
+                printf("Max request tries reached, giving up\n");
+                // Max tries reached, give up requesting mutex
+                break;
+            }
             // reinitialize open drain pin
             gpio_od_init(current_mutex_pin);
 
@@ -45,6 +55,7 @@ void mutex_handler_request_mutex()
                     currently_having_mutex = true;
                 }
             }
+            tries++;
         }
         // now we have the mutex, line can be released
         gpio_reset(current_mutex_pin);
@@ -61,12 +72,18 @@ void mutex_handler_request_mutex()
                 // if we receive a request then send a allow signal
                 if (received == MUTEX_REQEST)
                 {
+                    printf("received mutex request\n");
                     manchester_transmit_array(&ack, 1);
                     // now the other device has the mutex so we can reset the open drain pin
                     gpio_reset(current_mutex_pin);
+                    gpio_input_init(current_mutex_pin, GPIO_PULL_NONE );
+                    // Configure pin with no pull resistors to wait for release signal
+                    
                 }
                 else if (received == MUTEX_RELEASE)
                 {
+                    printf("received mutex release\n");
+                    gpio_od_init(current_mutex_pin);
                     currently_having_mutex = true;
                     manchester_transmit_array(&ack, 1);
                 }
@@ -88,10 +105,18 @@ void mutex_handler_release_mutex()
         // reinitialize open drain pin
         gpio_od_init(current_mutex_pin);
         bool released_permitted = false;
+        uint8_t tries = 0;
 
         while (!released_permitted)
         {
+            if (tries >= MAX_RELEASE_TRIES)
+            {
+                printf("Max release tries reached, giving up\n");
+                // Max tries reached, give up releasing mutex
+                break;
+            }
             uint8_t release = MUTEX_RELEASE;
+            printf("releasing mutex\n");
             manchester_transmit_array(&release, 1);
             uint8_t received;
             if (manchester_receive_array(&received, 1))
@@ -102,6 +127,7 @@ void mutex_handler_release_mutex()
                     currently_having_mutex = false;
                 }
             }
+            tries++;
         }
         // now reset the line to normal state
         gpio_reset(current_mutex_pin);
