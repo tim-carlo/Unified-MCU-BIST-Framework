1c9d65b596806b49be506066c02bbc95f2eff747
Päpke, Tim Carlo <tim.paepke@stud.tu-darmstadt.de>
2025-11-01 11:35:48 +0100

    code refactoring and removed udata in header

--- common/include/spooky_decoder.h
+++ common/include/spooky_decoder.h
@@ -11,35 +11,37 @@
 
 /* Callback, called when data is received.
  * UDATA is an arbitrary pointer for user data. */
-typedef void (spooky_decoder_cb)(uint8_t *data, uint8_t data_size, void *udata);
+typedef void(spooky_decoder_cb)(uint8_t *data, uint8_t data_size);
 
-struct spooky_decoder {
-    uint16_t index;             /* current index in buffer */
-    uint8_t buffer_size;        /* buffer size, in bytes */
-    uint8_t mode;               /* current state */
-    uint8_t ticks;              /* ticks since last logic level change */
-    uint8_t bit_index;          /* index of current bit in bit_accum */
-    uint8_t bit_accum;          /* accumulator for signal bits */
-    uint8_t last;               /* last bit received */
-    uint8_t interval;           /* avg. interval between single edges */
-    uint8_t payload_length;     /* bytes in payload */
-    uint8_t chksum;             /* sum-and-invert checksum for payload */
-    uint8_t pre_ticks;          /* tick count during setup part of bit frame */
+struct spooky_decoder
+{
+    uint16_t index;         /* current index in buffer */
+    uint8_t buffer_size;    /* buffer size, in bytes */
+    uint8_t mode;           /* current state */
+    uint8_t ticks;          /* ticks since last logic level change */
+    uint8_t bit_index;      /* index of current bit in bit_accum */
+    uint8_t bit_accum;      /* accumulator for signal bits */
+    uint8_t last;           /* last bit received */
+    uint8_t interval;       /* avg. interval between single edges */
+    uint8_t payload_length; /* bytes in payload */
+    uint8_t chksum;         /* sum-and-invert checksum for payload */
+    uint8_t pre_ticks;      /* tick count during setup part of bit frame */
 
     /* internal buffer, used for clock recovery and to accumulate payload */
     uint8_t *buffer;
-    spooky_decoder_cb *cb;      /* callback for successful data RX */
-    void *cb_udata;             /* void * userdata for callback */
+    spooky_decoder_cb *cb; /* callback for successful data RX */
 };
 // Structs packed
 
-enum spooky_decoder_init_res {
+enum spooky_decoder_init_res
+{
     SPOOKY_DECODER_INIT_OK = 0,
     SPOOKY_DECODER_INIT_ERROR_NULL = -1,
     SPOOKY_DECODER_INIT_ERROR_BAD_ARGUMENT = -2,
 };
 
-enum spooky_decoder_step_res {
+enum spooky_decoder_step_res
+{
     SPOOKY_DECODER_STEP_OK = 0,
     SPOOKY_DECODER_STEP_DONE = 1,
     SPOOKY_DECODER_STEP_ERROR_NULL = -1,
@@ -48,8 +50,8 @@ enum spooky_decoder_step_res {
 /* Initialize a spooky decoder. */
 enum spooky_decoder_init_res
 spooky_decoder_init(struct spooky_decoder *dec,
-    uint8_t *output_buffer, size_t buffer_size,
-    spooky_decoder_cb *cb, void *udata);
+                    uint8_t *output_buffer, size_t buffer_size,
+                    spooky_decoder_cb *cb);
 
 /* Step the decoder, with a new bit of input.
  * If a complete message has been received, the callback
