e7931d57944421aa8c633088faf6e1d551dfee94
Päpke, Tim Carlo <tim.paepke@stud.tu-darmstadt.de>
2025-11-05 00:25:51 +0100

    is not necessary

--- hal/msp430/msp430fr5994_helper.h
+++ hal/msp430/msp430fr5994_helper.h
@@ -21,21 +21,6 @@
 #define UART_PIN_TX ABS_PIN(2,0) // P2.0 (gpio 8)
 #define UART_PIN_RX ABS_PIN(2,1) // P2.1 (gpio 9)
 
-// Register offsets from port base
-#define PORT_IN_OFFSET 0x00
-#define PORT_OUT_OFFSET 0x02
-#define PORT_DIR_OFFSET 0x04
-#define PORT_REN_OFFSET 0x06
-#define PORT_SEL0_OFFSET 0x0A
-#define PORT_SEL1_OFFSET 0x0C
-#define PORT_SEL2_OFFSET 0x0E 
-#define PORT_SEL3_OFFSET 0x10 
-#define PORT_SEL4_OFFSET 0x12 
-#define PORT_SEL5_OFFSET 0x14 
-#define PORT_SEL6_OFFSET 0x16 
-#define PORT_SEL7_OFFSET 0x18 
-#define PORT_SEL8_OFFSET 0x1A 
-
 // Base address of the device descriptor table
 #define DEVICE_DESCRIPTOR_ADDR 0x1A00
 
