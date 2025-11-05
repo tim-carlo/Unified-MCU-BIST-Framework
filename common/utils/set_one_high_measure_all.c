3c0c3064be37568a1175a74ee66e4ac926edb656
Päpke, Tim Carlo <tim.paepke@stud.tu-darmstadt.de>
2025-11-05 14:24:29 +0100

    updated threshold for measuring and modified pin state logging logic

--- common/utils/set_one_high_measure_all.c
+++ common/utils/set_one_high_measure_all.c
@@ -16,8 +16,7 @@ static const uint32_t POLLING_DELAY_US = 1000;
 static const uint8_t threshold_percent = 70;
 static const uint8_t threshold = (NUMBER_OF_SAMPLES_FOR_DEBOUNCING * threshold_percent) / 100;
 
-
-static const uint8_t threshold_measure_percent = 70;
+static const uint8_t threshold_measure_percent = 100;
 static const uint8_t threshold_measure = (NUMBER_OF_SAMPLES_FOR_MEASURING * threshold_measure_percent) / 100;
 
 typedef uint8_t SetOneMeasureALLPhase;
@@ -85,7 +84,20 @@ static void flush_pin_states(const uint64_t blacklist_mask, gpio_pull_t pull_typ
     BitmapIterator it_reset = bitmap_iterator_create(~blacklist_mask);
     while (bitmap_iterator_next(&it_reset, &pin))
     {
-        gpio_reset(pin);
+        // gpio_reset(pin);
+        if (pull_type == GPIO_PULL_DOWN)
+            gpio_input_init(pin, GPIO_PULL_UP);
+        else if (pull_type == GPIO_PULL_UP)
+            gpio_input_init(pin, GPIO_PULL_DOWN);
+
+        // gpio_input_init(pin, pull_type);
+    }
+    delay_us(SETTLE_TIME_US);
+
+    BitmapIterator it_final = bitmap_iterator_create(~blacklist_mask);
+    while (bitmap_iterator_next(&it_final, &pin))
+    {
+        gpio_input_init(pin, GPIO_PULL_NONE);
     }
 }
 /**
@@ -108,61 +120,55 @@ static void reset_all_pins(uint64_t blacklist_mask)
 static void log_pin_changes(SetOneMeasureALLPhase phase,
                             PinData *pindata,
                             uint64_t blacklist_mask,
-                            uint64_t before_state,
-                            uint64_t after_state,
+                            uint32_t changes[64],
                             uint8_t test_pin)
 {
     BitmapIterator it = bitmap_iterator_create(~blacklist_mask);
     uint8_t pin;
     while (bitmap_iterator_next(&it, &pin))
     {
-        bool before = (before_state >> pin) & 1U;
-        bool after = (after_state >> pin) & 1U;
-
         if (pin == test_pin)
         {
-            // only store events to safe connection storage.
             switch (phase)
             {
             case PHASE_0_PULLDOWN_DRIVE_LOW:
-                if (after == 1)
+                if (changes[pin] >= threshold_measure)
                 {
+                    LOG("Phase %d: Pin %u NOT affected when driving low with pull-down\n", phase, pin);
                     add_pin_event(pindata, pin, PIN_IS_NOT_LOW_WHEN_PULLED_DOWN);
-                    printf("DEBUG: Pin %u did not go low when pulled down\n", pin);
-                }
-                break;
-            case PHASE_1_PULLUP_DRIVE_HIGH:
-                if (after == 0)
-                {
-                    add_pin_event(pindata, pin, PIN_IS_NOT_HIGH_WHEN_PULLED_UP);
-                    printf("DEBUG: Pin %u did not go high when pulled up\n", pin);
                 }
                 break;
             case PHASE_2_NO_PULL_DRIVE_LOW:
-                if (after == 1)
+                if (changes[pin] >= threshold_measure)
                 {
+                    LOG("Phase %d: Pin %u NOT affected when driving low with no pull\n", phase, pin);
                     add_pin_event(pindata, pin, PIN_IS_NOT_LOW_WHEN_DRIVEN_LOW);
-                    printf("DEBUG: Pin %u did not go low when driven low\n", pin);
                 }
-
+                break;
+            case PHASE_1_PULLUP_DRIVE_HIGH:
+                if (changes[pin] < threshold_measure)
+                {
+                    LOG("Phase %d: Pin %u NOT affected when driving high with pull-up\n", phase, pin);
+                    add_pin_event(pindata, pin, PIN_IS_NOT_HIGH_WHEN_PULLED_UP);
+                }
                 break;
             case PHASE_3_NO_PULL_DRIVE_HIGH:
-                if (after == 0)
+                if (changes[pin] < threshold_measure)
                 {
+                    LOG("Phase %d: Pin %u NOT affected when driving high with no pull\n", phase, pin);
                     add_pin_event(pindata, pin, PIN_IS_NOT_HIGH_WHEN_DRIVEN_HIGH);
-                    printf("DEBUG: Pin %u did not go high when driven high\n", pin);
                 }
                 break;
             default:
                 break;
             }
         }
-        else
+        else if (pin != test_pin)
         {
-            if (before != after)
+            if (changes[pin] >= threshold_measure)
             {
-                LOG("Pin %u changed when pin %u was changed\n", pin, test_pin);
-                add_pin_connection(CONNECTION_TYPE_INTERNAL, pindata, test_pin, pin, (uint8_t)phase);
+                LOG("Phase %d: Pin %u affected by pin %u\n", phase, pin, test_pin);
+                add_pin_connection(CONNECTION_TYPE_INTERNAL, pindata, pin, test_pin, (uint8_t)phase);
             }
         }
     }
@@ -194,36 +200,19 @@ static void phase_0_pulldown_drive_low(uint64_t blacklist_mask, PinData *pindata
 
             uint64_t after = read_all_pins(blacklist_mask);
 
+            uint64_t diff = (before ^ after) & ~blacklist_mask;
+
             for (uint8_t check_pin = 0; check_pin < 64; check_pin++)
             {
-                if (blacklist_mask & (1ULL << check_pin))
-                    continue;
-                bool before_state = (before >> check_pin) & 1U;
-                bool after_state = (after >> check_pin) & 1U;
-                if (before_state != after_state)
-                {
+                if (diff & (1ULL << check_pin))
                     changes[check_pin]++;
-                }
             }
 
-            gpio_reset(pin);
+            // gpio_reset(pin);
             flush_pin_states(blacklist_mask, GPIO_PULL_UP);
             delay_us(TIME_BETWEEN_MEASUREMENTS_US);
         }
-        uint64_t before_combined = 0;
-        uint64_t after_combined = 0;
-        for (uint8_t check_pin = 0; check_pin < 64; check_pin++)
-        {
-            if (blacklist_mask & (1ULL << check_pin))
-                continue;
-
-            if (changes[check_pin] >= threshold_measure)
-            {
-                after_combined |= (1ULL << check_pin);
-            }
-            before_combined |= (1ULL << check_pin);
-        }
-        log_pin_changes(PHASE_0_PULLDOWN_DRIVE_LOW, pindata, blacklist_mask, before_combined, after_combined, pin);
+        log_pin_changes(PHASE_0_PULLDOWN_DRIVE_LOW, pindata, blacklist_mask, changes, pin);
     }
 }
 
@@ -251,37 +240,19 @@ static void phase_1_pullup_drive_high(uint64_t blacklist_mask, PinData *pindata)
             delay_ms(20);
 
             uint64_t after = read_all_pins(blacklist_mask);
+            uint64_t diff = (before ^ after) & ~blacklist_mask;
 
             for (uint8_t check_pin = 0; check_pin < 64; check_pin++)
             {
-                if (blacklist_mask & (1ULL << check_pin))
-                    continue;
-                bool before_state = (before >> check_pin) & 1U;
-                bool after_state = (after >> check_pin) & 1U;
-                if (before_state != after_state)
-                {
+                if (diff & (1ULL << check_pin))
                     changes[check_pin]++;
-                }
             }
 
-            gpio_reset(pin);
+            // gpio_reset(pin);
             flush_pin_states(blacklist_mask, GPIO_PULL_DOWN);
             delay_us(TIME_BETWEEN_MEASUREMENTS_US);
         }
-        uint64_t before_combined = 0;
-        uint64_t after_combined = 0;
-        for (uint8_t check_pin = 0; check_pin < 64; check_pin++)
-        {
-            if (blacklist_mask & (1ULL << check_pin))
-                continue;
-
-            if (changes[check_pin] >= threshold_measure)
-            {
-                after_combined |= (1ULL << check_pin);
-            }
-            before_combined |= (1ULL << check_pin);
-        }
-        log_pin_changes(PHASE_1_PULLUP_DRIVE_HIGH, pindata, blacklist_mask, before_combined, after_combined, pin);
+        log_pin_changes(PHASE_1_PULLUP_DRIVE_HIGH, pindata, blacklist_mask, changes, pin);
     }
 }
 
@@ -311,38 +282,19 @@ static void phase_2_no_pull_drive_low(uint64_t blacklist_mask, PinData *pindata)
 
             uint64_t after = read_all_pins(blacklist_mask);
 
-            uint64_t change_mask = (before ^ after) & ~blacklist_mask;
+            uint64_t diff = (before ^ after) & ~blacklist_mask;
 
             for (uint8_t check_pin = 0; check_pin < 64; check_pin++)
             {
-                if (blacklist_mask & (1ULL << check_pin))
-                    continue;
-                bool before_state = (before >> check_pin) & 1U;
-                bool after_state = (after >> check_pin) & 1U;
-                if (before_state != after_state)
-                {
+                if (diff & (1ULL << check_pin))
                     changes[check_pin]++;
-                }
             }
 
-            gpio_reset(pin);
+            // gpio_reset(pin);
             flush_pin_states(blacklist_mask, GPIO_PULL_UP);
             delay_us(TIME_BETWEEN_MEASUREMENTS_US);
         }
-        uint64_t before_combined = 0;
-        uint64_t after_combined = 0;
-        for (uint8_t check_pin = 0; check_pin < 64; check_pin++)
-        {
-            if (blacklist_mask & (1ULL << check_pin))
-                continue;
-
-            if (changes[check_pin] >= threshold_measure)
-            {
-                after_combined |= (1ULL << check_pin);
-            }
-            before_combined |= (1ULL << check_pin);
-        }
-        log_pin_changes(PHASE_2_NO_PULL_DRIVE_LOW, pindata, blacklist_mask, before_combined, after_combined, pin);
+        log_pin_changes(PHASE_2_NO_PULL_DRIVE_LOW, pindata, blacklist_mask, changes, pin);
     }
 }
 
@@ -369,37 +321,19 @@ static void phase_3_no_pull_drive_high(uint64_t blacklist_mask, PinData *pindata
             delay_ms(40);
 
             uint64_t after = read_all_pins(blacklist_mask);
+            uint64_t diff = (before ^ after) & ~blacklist_mask;
 
             for (uint8_t check_pin = 0; check_pin < 64; check_pin++)
             {
-                if (blacklist_mask & (1ULL << check_pin))
-                    continue;
-                bool before_state = (before >> check_pin) & 1U;
-                bool after_state = (after >> check_pin) & 1U;
-                if (before_state != after_state)
-                {
+                if (diff & (1ULL << check_pin))
                     changes[check_pin]++;
-                }
             }
 
-            gpio_reset(pin);
+            // gpio_reset(pin);
             flush_pin_states(blacklist_mask, GPIO_PULL_DOWN);
             delay_us(TIME_BETWEEN_MEASUREMENTS_US);
         }
-        uint64_t before_combined = 0;
-        uint64_t after_combined = 0;
-        for (uint8_t check_pin = 0; check_pin < 64; check_pin++)
-        {
-            if (blacklist_mask & (1ULL << check_pin))
-                continue;
-
-            if (changes[check_pin] >= threshold_measure)
-            {
-                after_combined |= (1ULL << check_pin);
-            }
-            before_combined |= (1ULL << check_pin);
-        }
-        log_pin_changes(PHASE_3_NO_PULL_DRIVE_HIGH, pindata, blacklist_mask, before_combined, after_combined, pin);
+        log_pin_changes(PHASE_3_NO_PULL_DRIVE_HIGH, pindata, blacklist_mask, changes, pin);
     }
 }
 
